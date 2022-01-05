0600aaff8e1a64eaaaaef8a477f2b56c78640e0b
Yaël GUILLOUX <yael.guilloux@gmail.com>
2022-01-05 01:44:01 +0100

    feat(vitest): replace jest with vitest

--- tests/index.spec.ts
+++ tests/index.spec.ts
@@ -1,3 +1,4 @@
+import { describe, expect, it } from 'vitest'
 import { useToggle } from '../src'
 
 describe('useToggle', () => {
--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,10 @@
 import vue from '@vitejs/plugin-vue'
-import { resolve } from 'path'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
   root: 'demo/',
   plugins: [vue()],
-  alias: [
-    {
-      find: '@lib',
-      replacement: resolve(__dirname, './src/index.ts'),
-    },
-  ],
+  test: {
+    include: ['**/*.spec.ts']
+  }
 })
